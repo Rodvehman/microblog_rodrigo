0084ce627e18f2b2ab47ba41fafb7ef17870872e
Rodvehman <63985415+Rodvehman@users.noreply.github.com>
2025-11-18 17:06:18 -0300

    Update NoticiasServico

--- src/Services/NoticiaServico.php
+++ src/Services/NoticiaServico.php
@@ -10,7 +10,14 @@
 
         // Versão provisória (básica)
         public function buscar():array {
-            $sql = "SELECT * FROM noticias ORDER BY data DESC";
+            $sql = "SELECT 
+                        noticias.id,
+                        noticias.titulo,
+                        noticias.data,
+                        usuarios.nome AS autor
+                    FROM noticias JOIN usuarios
+                    ON noticias.usuario_id = usuarios.id
+                    ORDER BY data DESC";
             $consulta = $this->conexao->query($sql);
             return $consulta->fetchAll();
         }
