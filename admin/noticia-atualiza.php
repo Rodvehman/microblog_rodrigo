7c85101b8f2f9a1b7f43bf120790020a1eef101a
Rodvehman <63985415+Rodvehman@users.noreply.github.com>
2025-11-18 13:12:06 -0300

    Update noticia-atualiza.php

--- admin/noticia-atualiza.php
+++ admin/noticia-atualiza.php
@@ -1,53 +1,29 @@
-<?php
-require_once "../includes/cabecalho-admin.php";
+<?php 
+	require_once "../src/Helpers/Utils.php";
+    require_once "../src/Services/AutenticacaoServico.php";
 
+    AutenticacaoServico::exigirLogin();
+
+    require_once "../includes/cabecalho-admin.php";
 ?>
 
 
-<div class="row">
-    <article class="col-12 bg-white rounded shadow my-1 py-4">
+<article class="p-5 my-4 rounded-3 bg-white shadow">
+    <div class="container-fluid py-1">        
+        <h2 class="display-4 bg-warning rounded text-center">Não autorizado!</h2>
+        <hr class="my-4">
+        <p class="fs-5 text-center"><b> <?= $_SESSION['nome'] ?> </b>, você <span class="badge bg-danger">não tem permissão </span> para acessar este recurso.</p>
+        <hr class="my-4">
 
-        <h2 class="text-center">
-            Atualizar dados da notícia
-        </h2>
-
-        <form class="mx-auto w-75" action="" method="post" id="form-atualizar" name="form-atualizar" autocomplete="off">
-            <input type="hidden" name="id" value="id da notícia...">
-
-            <div class="mb-3">
-                <label class="form-label" for="titulo">Título:</label>
-                <input value="titulo da notícia..." class="form-control" type="text" id="titulo" name="titulo">
-            </div>
-
-            <div class="mb-3">
-                <label class="form-label" for="texto">Texto:</label>
-                <textarea class="form-control" name="texto" id="texto" cols="50" rows="6">texto da notícia...</textarea>
-            </div>
-
-            <div class="mb-3">
-                <label class="form-label" for="resumo">Resumo (máximo de 300 caracteres):</label>
-                <span id="maximo" class="badge bg-danger">0</span>
-                <textarea class="form-control" name="resumo" id="resumo" cols="50" rows="2" maxlength="300">resumo da notícia...</textarea>
-            </div>
-
-            <div class="mb-3">
-                <label for="imagem-existente" class="form-label">Imagem da notícia:</label>
-                <!-- campo somente leitura, meramente informativo -->
-                <input value="imagem da notícia..." class="form-control" type="text" id="imagem-existente" name="imagem-existente" readonly>
-            </div>
-
-            <div class="mb-3">
-                <label for="imagem" class="form-label">Caso queira mudar, selecione outra imagem:</label>
-                <input class="form-control" type="file" id="imagem" name="imagem" accept="image/png, image/jpeg, image/gif, image/svg+xml">
-            </div>
-
-            <button class="btn btn-primary" name="atualizar"><i class="bi bi-arrow-clockwise"></i> Atualizar</button>
-        </form>
-
-    </article>
-</div>
+        <p>
+            <a href="index.php" class="btn btn-primary">Voltar para a página inicial</a>
+        </p>
+        
+    </div>
+</article>
 
 
-<?php
+<?php 
 require_once "../includes/rodape-admin.php";
 ?>
+
