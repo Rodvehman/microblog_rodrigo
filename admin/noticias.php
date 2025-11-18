f30db2592969e37f88f1896a6cb8f8dba9c6480e
Rodvehman <63985415+Rodvehman@users.noreply.github.com>
2025-11-18 17:23:48 -0300

    Update noticias.php

--- admin/noticias.php
+++ admin/noticias.php
@@ -57,13 +57,13 @@
 						<td class="text-center"></td>	
 						<td class="text-center">
 							<a class="btn btn-warning" 
-							href="noticia-atualiza.php">
+							href="noticia-atualiza.php?id=<?= $noticia['id'] ?>">
 							<i class="bi bi-pencil"></i> Atualizar
 							</a>
 						</td>
 						<td>
 							<a class="btn btn-danger excluir" 
-							href="noticia-exclui.php">
+							href="noticia-exclui.php?id=<?= $noticia['id'] ?>">
 							<i class="bi bi-trash"></i> Excluir
 							</a>
 			<?php endforeach; ?>
