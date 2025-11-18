1f6126aef6aebd60c91a4391fa99a294f0c186f3
Rodvehman <63985415+Rodvehman@users.noreply.github.com>
2025-11-18 13:36:01 -0300

    Update microblog_rodrigo.sql

--- src/Database/microblog_rodrigo.sql
+++ src/Database/microblog_rodrigo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 04/11/2025 às 18:14
+-- Tempo de geração: 18/11/2025 às 17:34
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `microblog_joao`
+-- Banco de dados: `microblog_rodrigo`
 --
 
 -- --------------------------------------------------------
@@ -52,6 +52,14 @@ CREATE TABLE `usuarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Despejando dados para a tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `tipo`) VALUES
+(2, 'Rodrigo Alexandre Vehman', 'rodvehman@hotmail.com', '$2y$10$5kf/oMxLbXLu5LRA6mKsG.qUboDOdpMzfmq8LU50W.S9w85dUY0me', 'admin'),
+(4, 'Editor', 'editor@editor', '$2y$10$QQL6vCtedsKW.U8I8eJ8hOpYtM31.3TMgQPb79gyhMjVT4cF87XVu', 'editor');
+
+--
 -- Índices para tabelas despejadas
 --
 
@@ -83,7 +91,7 @@ ALTER TABLE `noticias`
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restrições para tabelas despejadas
