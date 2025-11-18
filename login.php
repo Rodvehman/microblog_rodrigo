d8c28d382fff1bdbb97a7b17d56bb87b8056a388
Rodvehman <63985415+Rodvehman@users.noreply.github.com>
2025-11-18 16:11:03 -0300

    Cria método buscar (provisório)

--- login.php
+++ login.php
@@ -1,10 +1,65 @@
 <?php 
+require_once "src/Database/Conecta.php";
+require_once "src/Services/UsuarioServico.php";
+require_once "src/Helpers/Utils.php";
+require_once "src/Services/AutenticacaoServico.php";
+
+$usuarioServico = new UsuarioServico();
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+    if(empty($_POST['email']) || empty($_POST['senha'])){
+        Utils::redirecionarPara("login.php?campos_obrigatorios");
+    } else {
+        // Captura e-mail e senha
+        $email = Utils::sanitizar($_POST['email'], 'email');
+        $senha = $_POST['senha'];
+
+        // Busca pelo usuário através do e-mail
+        $dadosDoUsuario = $usuarioServico->buscarPorEmail($email);
+
+        // Se não existir usuário/usuário inválido, redirecione para login.php
+        if(!$dadosDoUsuario){
+            Utils::redirecionarPara("login.php?dados_incorretos");
+        } else {
+            // Caso contrário, verifique a senha
+            if( password_verify( $senha, $dadosDoUsuario['senha'] ) ){
+                // Estando correta, faça o login
+                AutenticacaoServico::login(
+                    $dadosDoUsuario['id'],
+                    $dadosDoUsuario['nome'],
+                    $dadosDoUsuario['tipo']
+                );
+            } else{
+                // Estando errada, mantenha em login.php
+                Utils::redirecionarPara("login.php?dados_incorretos");
+            }            
+        }
+    }
+
+}
+
+/* Mensagens do processo de login/logout */
+if(isset($_GET['acesso_proibido'])){
+    $mensagem = "Você deve logar primeiro";
+} elseif(isset($_GET['campos_obrigatorios'])){
+    $mensagem = "Preencha e-mail e senha";
+} elseif(isset($_GET['dados_incorretos'])){
+    $mensagem = "Algo de errado não está certo";
+} elseif(isset($_GET['saiu'])){
+    $mensagem = "Você saiu do sistema";
+}
+
 require_once "includes/cabecalho.php";
 ?>
 
 <div class="row">
     <div class="bg-white rounded shadow col-12 my-1 py-4">
         <h2 class="text-center fw-light">Acesso à área administrativa</h2>
+
+        <?php if(isset($mensagem)):?>
+            <p class="alert alert-warning text-center my-2"><?=$mensagem?></p>
+        <?php endif;?>
 
         <form action="" method="post" id="form-login" name="form-login" class="mx-auto w-50" autocomplete="off">
 			
