9fb7688e0769e1700b872646ca099292500215f5
Rodvehman <63985415+Rodvehman@users.noreply.github.com>
2025-11-19 17:20:30 -0300

    Update noticia-atualiza.php

--- admin/noticia-atualiza.php
+++ admin/noticia-atualiza.php
@@ -2,7 +2,7 @@
 
 require_once "../src/Database/Conecta.php";
 require_once "../src/Models/Usuario.php";
-require_once "../src/Services/UsuarioServico.php";
+require_once "../src/Services/NoticiaServico.php";
 require_once "../src/Helpers/Utils.php";
 require_once "../src/Services/AutenticacaoServico.php";
 AutenticacaoServico::exigirLogin();
@@ -11,10 +11,10 @@ $erro = null;
 $noticiaServico = new NoticiaServico();
 
 $id = Utils::sanitizar($_GET['id'], 'inteiro');
-if ($id) Utils::redirecionarPara('noticias.php');
+if (!$id) Utils::redirecionarPara('noticias.php');
 try {
     $dados = $noticiaServico->buscarPorId($id, $_SESSION['tipo'], $_SESSION['id']);
-    if (!$dados) $erro = "Notícia n]ao encontrada";
+    if (!$dados) $erro = "Notícia não encontrada";
     Utils::dump($dados);
 } catch (\Throwable $e) {
     $erro = "Erro ao buscar dados da notícia.<br>".$e->getMessage();
