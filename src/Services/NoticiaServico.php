191e1456480a0802f08c08f38c2aa7e8432302ff
Rodvehman <63985415+Rodvehman@users.noreply.github.com>
2025-11-26 14:15:33 -0300

    Update NoticiaServico.php

--- src/Services/NoticiaServico.php
+++ src/Services/NoticiaServico.php
@@ -131,4 +131,23 @@
             $consulta = $this->conexao->query($sql);
             return $consulta->fetchAll();
         }
+
+        // 
+        public function exibirNoticiaCompleta(int $idNoticia):array {
+            $sql = "SELECT
+                        noticias.id,
+                        noticias.titulo,
+                        noticias.data,
+                        noticias.texto,
+                        noticias.imagem,
+                        usuarios.nome AS autor
+                    FROM noticias JOIN usuarios
+                    ON noticias.usuario_id = usuarios.id
+                    WHERE noticias.id = :id";
+                    
+            $consulta =$this->conexao->prepare($sql);
+            $consulta->bindValue(":id", $idNoticia);
+            $consulta->execute();
+            return $consulta->fetch();
+        }
     }
